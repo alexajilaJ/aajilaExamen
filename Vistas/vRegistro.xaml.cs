c16e9a8eb608bd75c6c3a79381564ccbf61b52e1
Alex <Alex@DESKTOP-DNAHFBI>
2024-04-25 20:36:55 -0500

    ventana

--- Vistas/vRegistro.xaml.cs
+++ Vistas/vRegistro.xaml.cs
@@ -2,6 +2,7 @@ namespace aajilaExamen.Vistas;
 
 public partial class vRegistro : ContentPage
 {
+    int pago = 1500;
     public string Username { get; set; }
     public vRegistro()
 	{
@@ -11,7 +12,10 @@ public partial class vRegistro : ContentPage
 
     private void btnCalculo_Clicked(object sender, EventArgs e)
     {
-
+        int pag = Convert.ToInt32(txtMonto.Text);
+        int resto = 1500 - pag;
+        double tota = (resto / 4) + (1500 * 0.04);
+        txtPago.Placeholder = tota.ToString();
     }
     private void btnResumen_Clicked(object sender, EventArgs e)
     {
